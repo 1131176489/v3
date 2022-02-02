2415a9e88d33ca203e5522fdd8980b4b801b2ccd
1131176489 <62178487+1131176489@users.noreply.github.com>
2022-02-03 02:55:43 +0800

    安装less和less-loader
    安装了less和less-loader，注意要安装对应版本的

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="login_container">
         登录组件
     </div>
 </template>
@@ -11,5 +11,7 @@ export default {}
 </script>
 
 <style lang="less" scoped>
-
+.login_container {
+    background-color: #2b4b6b;
+}
 </style>
