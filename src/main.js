978bacdf4d37463d5a17c7be46b32c3608c27791
1131176489 <62178487+1131176489@users.noreply.github.com>
2022-02-03 22:13:12 +0800

    完成了一系列的功能，到表单验证
    完成了一系列的功能，到表单验证

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import './plugins/element.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/icomoon/style.css'
 
 Vue.config.productionTip = false
 
