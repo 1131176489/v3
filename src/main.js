67a6bba455855fbc118ce2cc15c41c070dc5bd22
1131176489 <62178487+1131176489@users.noreply.github.com>
2022-02-03 23:35:15 +0800

    配置axios和登录的Ajax

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import './assets/css/global.css'
 // 导入字体图标
 import './assets/icomoon/style.css'
 
+import axios from 'axios'
+// 配置请求的路径
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
