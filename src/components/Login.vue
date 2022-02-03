2f22d0136844bf2159fe25610b87d00481d0ce5f
1131176489 <62178487+1131176489@users.noreply.github.com>
2022-02-03 22:23:21 +0800

    登录按钮功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -30,7 +30,7 @@
         <!-- 按钮 -->
         <el-form-item class="btns">
           <el-row>
-            <el-button type="primary">登录</el-button>
+            <el-button type="primary" @click="login">登录</el-button>
             <el-button type="info" @click="resetLoginForm">重置</el-button>
           </el-row>
         </el-form-item>
@@ -67,6 +67,11 @@ export default {
     resetLoginForm () {
     //   console.log(this)
       this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate((valid) => {
+        console.log(valid)
+      })
     }
   }
 }
